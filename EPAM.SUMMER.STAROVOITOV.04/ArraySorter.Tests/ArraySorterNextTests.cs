104d3a39721808ae4925b5991c396cc9608b6e8c
VladislavStarovoitov <VladislavStarovoitov>
2016-07-14 23:33:56 +0300

    Add some nunit tests for new class with sort

---
+++ EPAM.SUMMER.STAROVOITOV.04/ArraySorter.Tests/ArraySorterNextTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ArraySorter.Tests
+{
+    [TestFixture]
+    class ArraySorterNextTests
+    {
+        [TestCaseSource(typeof(DataProvider), nameof(DataProvider.GetArrayForSortingBySumAscWithDelegateComparer))]
+        public void SortBySumAscWithDelegateComparer(int[][] testArray, int[][] sortedArray)
+        {
+            Func<int[], int[], int> comparer = DelegateComparer.SortBySumAsc;
+
+            JaggedArraySorterNext.SortJaggedArray(testArray, comparer);
+
+            CollectionAssert.AreEqual(sortedArray, testArray);
+        }
+
+        [TestCaseSource(typeof(DataProvider), nameof(DataProvider.GetArrayForSortingBySumAscWithDelegateComparer))]
+        public void SortBySumAscWithInterfaceComparer(int[][] testArray, int[][] sortedArray)
+        {
+            var comparer = new SumAscComparer();
+
+            JaggedArraySorterNext.SortJaggedArray(testArray, comparer);
+
+            CollectionAssert.AreEqual(sortedArray, testArray);
+        }
+
+        public static class DataProvider
+        {
+            public static IEnumerable GetArrayForSortingBySumAscWithDelegateComparer()
+            {
+                yield return new TestCaseData(new int[3][] { new int[] { 1, 2, 3 }, new int[] { 1, 2 }, new int[] { 4, 15, 9 } },
+                                              new int[3][] { new int[] { 1, 2 }, new int[] { 1, 2, 3 }, new int[] { 4, 15, 9 } });
+            }
+        }
+    }
+}
