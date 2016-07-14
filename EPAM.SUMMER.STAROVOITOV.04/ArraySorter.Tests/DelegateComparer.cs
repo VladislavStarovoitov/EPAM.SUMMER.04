93c00ca4f88f6147bcb8d6fc012e1d7c8e070c77
VladislavStarovoitov <VladislavStarovoitov>
2016-07-14 23:35:27 +0300

    Add static class with sort methods

---
+++ EPAM.SUMMER.STAROVOITOV.04/ArraySorter.Tests/DelegateComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySorter.Tests
+{
+    public static class DelegateComparer
+    {
+        public static int SortBySumAsc(int[] x, int[] y)
+        {
+            if (x.Sum() > y.Sum())
+                return 1;
+            if (x.Sum() < y.Sum())
+                return -1;
+            return 0;
+        }
+    }
+}
